a9cea61516dcd001a37ff4f8c7fb6c8d65e69c4c
aghiyaghaniiy <ghaniyalma@gmail.com>
2026-01-21 11:28:01 +0700

    belajar php

---
+++ Belajar.php/hello.php
@@ -0,0 +1,12 @@
+<?php
+echo "<h2><b>Biodata Siswa</b></h2>";
+
+echo "Hello World!<br>";
+echo "Nama : Alma Hamida Ghaniiy<br>";
+echo "Tempat Tanggal Lahir: Bandung 31-01-2009<br>";
+echo "Umur: 17 tahun<br>";
+echo "Kelas: XI TJKT 1<br>";
+echo "Jenis Kelamin: Perempuan<br>";
+echo "Agama : Islam<br>";
+echo "Alamat :Jalan Raya Bojongsoang Kp.Lengkong Rt 03/Rw 01 Desa Lengkong<br>";
+?>
---
+++ img/belajar.php/hello.php
@@ -0,0 +1,9 @@
+<?php
+echo "<h2><b>Biodata Siswa</b></h2>";
+
+echo "Hello World!<br>";
+echo "Nama : Alma Hamida Ghaniiy";
+echo "Tempat : bandung 31-01-2009";
+echo "Agama : Islam<br>";
+echo "Alamat :Jalan Raya Bojongsoang Kp.Lengkong Rt 03/Rw 01 Desa Lengkong<br>";
+
