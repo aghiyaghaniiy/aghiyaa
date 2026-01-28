26035750089f84eae5816a589a6b9942a5de5295
aghiyaghaniiy <ghaniyalma@gmail.com>
2026-01-28 22:13:50 +0700

    baru

---
+++ Belajar.php/operatorRelasi.php
@@ -0,0 +1,28 @@
+<?php
+
+$a = 6;
+$b = 2;
+
+$c = $a > $b;
+echo "$a > $b: $c";
+echo "<hr>";
+
+$c = $a < $b;
+echo "$a < $b: $c";
+echo "<hr>";
+
+$c = $a == $b;
+echo "$a == $b: $c";
+echo "<hr>";
+
+$c = $a != $b;
+echo "$a != $b: $c";
+echo "<hr>";
+
+$c = $a >= $b;
+echo "$a >= $b: $c";
+echo "<hr>";
+
+$c = $a <= $b;
+echo "$a <= $b: $c";
+echo "<hr>";
