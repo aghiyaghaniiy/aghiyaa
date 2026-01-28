5e2de5625d773214d256c237f61d922fcfe16a77
aghiyaghaniiy <ghaniyalma@gmail.com>
2026-01-28 09:02:29 +0700

    baru

---
+++ Belajar.php/belajarvariabel.php
@@ -0,0 +1,26 @@
+<?php
+
+// tipe data char (karakter)
+$jenis_kelamin = 'p';
+
+// tipe data string (teks)
+$nama_lengkap = "Alma Hamida Ghaniiy";
+
+// tipe data integer
+$umur =17 ;
+
+// tipe data float
+$berat = 47;
+// tipe data float
+$tinggi = 158;
+
+// tipe data boolean
+$menikah ="Belum menikah masih pelajar";
+
+echo "Nama: $nama_lengkap<br>";
+echo "Jenis Kelamin: $jenis_kelamin<br>";
+echo "Umur: $umur tahun<br>";
+echo "berat badan: $berat kg<br>";
+echo "tinggi badan: $tinggi cm<br>";
+echo "menikah: $menikah";
+    ?>
