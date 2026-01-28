046bfe3e3ee10c997e541d855dbf97bde66335db
aghiyaghaniiy <ghaniyalma@gmail.com>
2026-01-28 09:09:19 +0700

    baru

---
+++ Belajar.php/operatoraritmatika.php
@@ -0,0 +1,36 @@
+<?php
+
+$a = 26;
+$b = 01;
+
+// penjumlahan
+$c = $a + $b;
+echo "$a + $b = $c";
+echo "<hr>";
+
+// pengurangan
+$c = $a - $b;
+echo "$a - $b = $c";
+echo "<hr>";
+
+// Perkalian
+$c = $a * $b;
+echo "$a * $b = $c";
+echo "<hr>";
+
+// Pembagian
+$c = $a / $b;
+echo "$a / $b = $c";
+echo "<hr>";
+
+// Sisa bagi
+$c = $a % $b;
+echo "$a % $b = $c";
+echo "<hr>";
+
+// Pangkat
+$c = $a ** $b;
+echo "$a ** $b = $c";
+echo "<hr>";
+
+?>
