b46f92162706e302024c16f86e0a7b5f42802902
aghiyaghaniiy <ghaniyalma@gmail.com>
2026-01-28 22:33:29 +0700

    new

---
+++ Belajar.php/OperatorTernary.php
@@ -0,0 +1,9 @@
+<?php
+
+$suka = true;
+
+$jawab = $suka
+    ? "Data berhasil diproses dan disimpan ke dalam sistem"
+    : "Data gagal diproses dan tidak disimpan ke dalam sistem";
+
+echo $jawab; 
